e45e717409b336b96a3aaae2421757b16236e949
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-20 00:50:41 +0300

    make the print environment function ./environ

---
+++ k_work/environ.c
@@ -0,0 +1,18 @@
+/* #include "main.h" */
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv, char **envp)
+{
+	extern char **environ;
+	int i = 0;
+	
+	while(environ[i])
+	{
+	       	printf("%s\n", environ[i++]); /* prints in form of "variable=value" */
+	}
+	return (0);
+					   
+}
