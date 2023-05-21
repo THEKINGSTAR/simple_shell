d23e56ad9c5c485f68b09c56bf54bdd77cfc4145
m0hamm8d <mm994225@gmail.com>
2023-05-21 13:52:10 -0700

    just one

--- main.c
+++ main.c
@@ -1,22 +1,42 @@
 #include "shell.h"
 
 /**
- * main - entry point
- *
- * Return: 0 if success or 1 others
- */
+* main - entry point
+*
+* Return: 0 if success or 1 others
+*/
 int main(void)
 {
+	int i;
 	char *line;
 	char **line_splited;
+	char **path;
+	char *x;
+	const char *envp[] = {NULL};
 
-	do {
+	while(1)
+	{
 		printf("($) ");
 		line = get_line();
 		line_splited = split(line);
-		exe(line_splited);
-		free(line);
-		free(line_splited);
-	} while (1);
-	return (EXIT_SUCCESS);
+		path = handle_path();
+		for (i = 0; path[i] != NULL; i++)
+		{
+			if ((find_file(path[i], line_splited[0])) == 1)
+			{
+				strcat(path[i], "/");
+				strcat(path[i], line_splited[0]);
+				break;
+			}
+		}
+		if (fork() == 0)
+		{
+			execve(path[i], line_splited, (char * const *)envp);
+			printf("%s %s\n", path[i], line_splited[0]);
+			perror("hsh");
+			exit(EXIT_FAILURE);
+		}
+		wait(NULL);
+	}
+		return (0);
 }
