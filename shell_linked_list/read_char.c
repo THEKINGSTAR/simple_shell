570888cb0dddf210bee397fe838e0340b0512d00
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-26 15:53:20 +0300

    uploading all files needed

---
+++ shell_linked_list/read_char.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ * read_line_by_char - read by character
+ * Return: return the readed
+ */
+char *read_line_by_char(void)
+{
+	int bufsize = 1024;
+	char *line;
+	int c;
+	int position = 0;
+	size_t nread, size;
+
+	line = malloc(sizeof(char) * bufsize);
+	if (line == NULL)
+	{
+		fprintf(stderr, "error to alloc memory::read_line_char");
+		exit(EXIT_FAILURE);
+	}
+	nread = getline(&line, &size, stdin);
+	if (!nread)
+	{
+		fprintf(stderr, "error to alloc memory::read_lin::char");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		c = line[position];
+		if (c == EOF || c == '\n')
+		{
+			line[position] = '\0';
+			return (line);
+		}
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += 1024;
+			line = realloc(line, bufsize);
+			if (line == NULL)
+			{
+				fprintf(stderr, "error to alloc memory:read_line_char");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
