ae7027211c7e3a47c16334ae3811180b6bb9ca23
m0hamm8d <mm994225@gmail.com>
2023-05-27 14:23:55 +0300

    try

---
+++ _printenvp.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+* _printenv - print enviroment
+* @envp: 1
+*
+* Return: non
+*/
+void _printenv(char **envp)
+{
+	size_t run = 0;
+
+	while (envp[run])
+	{
+		write(STDOUT_FILENO, envp[run], _strlen(envp[run]));
+		write(STDOUT_FILENO, "\n", 1);
+		run++;
+	}
+}
+
